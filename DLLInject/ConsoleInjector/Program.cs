ddb1db58e43d6b069bc2461000234a9122369e7a
Yury <xismatulin.ru@yandex.ru>
2018-12-07 16:39:35 +0500

    AddDllInjector

---
+++ DLLInject/ConsoleInjector/Program.cs
@@ -0,0 +1,38 @@
+﻿using Injector;
+using System;
+using System.IO;
+using System.Reflection;
+
+namespace alphabotcsharp
+{
+
+    public class Program
+    {
+        
+
+        public static void Main()
+        {
+            string rawDLL = String.Empty;
+            if (Injection.is64BitOperatingSystem)
+            {
+                rawDLL = Path.Combine(Path.GetDirectoryName(Assembly.GetEntryAssembly().Location), "DLLInject_64.dll");
+            }
+            else
+            {
+                rawDLL = Path.Combine(Path.GetDirectoryName(Assembly.GetEntryAssembly().Location), "DLLInject_64.dll");
+            }
+            DllInjectionResult result = Injection.Inject("mspaint", rawDLL);
+            Console.WriteLine(result);         
+            Console.ReadLine();
+        }
+    }
+
+    public enum DllInjectionResult
+    {
+        DllNotFound,
+        GameProcessNotFound,
+        InjectionFailed,
+        Success
+    }
+
+}
